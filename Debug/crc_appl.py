816935ba692c21e65bc72f24f90ec237e8ec479e
Paul Rojas <sirpaul.tapia@hotmail.com>
2026-05-12 18:28:42 +0200

    [ARM_M4_RTOS_BL_Validate_APPL] Python script correction
    - Previous commit contains wrong python script with MPEG crc generation.
    
    - This file generates the proper CRC and Size tested on both Bootloader
      and application

--- Debug/crc_appl.py
+++ Debug/crc_appl.py
@@ -1,14 +1,11 @@
 import zlib
-import crc32_mpeg2
 
 APP_BIN = "ARM_M4_RTOS_NO_HEADER.bin"
 
 with open(APP_BIN, "rb") as f:
     data = f.read()
 
-# 2. Calculate CRC on ONLY the app data
-
-crc = crc32_mpeg2(data)
+crc = zlib.crc32(data) & 0xFFFFFFFF
 
 print(f"New CRC = 0x{crc:08X}")
 print(f"Size = {len(data)}bytes")
